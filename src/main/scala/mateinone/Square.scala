0e30afb3e696d387c97720105a9bf4c59183ca93
Dave Swartz <dwswartz@gmail.com>
2014-02-16 14:00:39 -0500

    Used comma delimited initializer to simplify Square static data initialization.

--- src/main/scala/mateinone/Square.scala
+++ src/main/scala/mateinone/Square.scala
@@ -30,27 +30,17 @@ case class Rank private(n: Int) extends Line[Rank] {
 }
 
 object Square {
-  // TODO get rid of fileSquares, change 8 tuple initializers to a1, a2 .. h8 = next(Square.apply)
-  import File._
-  import Rank._
-  private def fileSquares(f: File) = (Square(f,_1), Square(f,_2), Square(f,_3), Square(f,_4), Square(f,_5), Square(f,_6), Square(f,_7), Square(f,_8))
-  val (a1, a2, a3, a4, a5, a6, a7, a8) = fileSquares(A)
-  val (b1, b2, b3, b4, b5, b6, b7, b8) = fileSquares(B)
-  val (c1, c2, c3, c4, c5, c6, c7, c8) = fileSquares(C)
-  val (d1, d2, d3, d4, d5, d6, d7, d8) = fileSquares(D)
-  val (e1, e2, e3, e4, e5, e6, e7, e8) = fileSquares(E)
-  val (f1, f2, f3, f4, f5, f6, f7, f8) = fileSquares(F)
-  val (g1, g2, g3, g4, g5, g6, g7, g8) = fileSquares(G)
-  val (h1, h2, h3, h4, h5, h6, h7, h8) = fileSquares(H)
-  val squares: Vector[Square] = Vector(a1, a2, a3, a4, a5, a6, a7, a8,
-                                       b1, b2, b3, b4, b5, b6, b7, b8,
-                                       c1, c2, c3, c4, c5, c6, c7, c8,
-                                       d1, d2, d3, d4, d5, d6, d7, d8,
-                                       e1, e2, e3, e4, e5, e6, e7, e8,
-                                       f1, f2, f3, f4, f5, f6, f7, f8,
-                                       g1, g2, g3, g4, g5, g6, g7, g8,
-                                       h1, h2, h3, h4, h5, h6, h7, h8)
-  def square(file: File, rank: Rank): Square = squares(file.n * 8 + rank.n)
+  val squares: Vector[Vector[Square]] = File.files.map(f => Rank.ranks.map(r => Square(f, r)))
+  private val squareIter = squares.flatten.iterator
+  val a1, a2, a3, a4, a5, a6, a7, a8,
+      b1, b2, b3, b4, b5, b6, b7, b8,
+      c1, c2, c3, c4, c5, c6, c7, c8,
+      d1, d2, d3, d4, d5, d6, d7, d8,
+      e1, e2, e3, e4, e5, e6, e7, e8,
+      f1, f2, f3, f4, f5, f6, f7, f8,
+      g1, g2, g3, g4, g5, g6, g7, g8,
+      h1, h2, h3, h4, h5, h6, h7, h8 = squareIter.next()
+  def square(file: File, rank: Rank): Square = squares(file.n)(rank.n)
 }
 case class Square private(file: File, rank: Rank) {
   def +(rhs: (Int, Int)): Option[Square] = (file + rhs._1).flatMap(fo => (rank + rhs._2).map(ro => Square.square(fo, ro)))
