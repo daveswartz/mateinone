ae4ba0d6c60ef1ba6fc34c4ef689dfafc1ef1e62
Dave Swartz <dwswartz@gmail.com>
2014-04-27 13:48:39 -0400

    Using parallel collections and storing scores that have already been computed.

--- script/evaluation.scala
+++ script/evaluation.scala
@@ -1,29 +1,33 @@
+import java.util.concurrent._
+import collection.JavaConverters._
 import mateinone._
 import TerminalPrinter._
 import MovePrinter._
 
-def value(pieces: Vector[Piece]) = pieces.map(_.`type`).map {
-  case Pawn => 100
-  case Knight => 350
-  case Bishop => 350
-  case Rook => 525
-  case Queen => 1000
-  case King => 0
-}.sum
+def score(board: Board) = {
+  def value(pieces: Vector[Piece]) = pieces.map(_.`type`).map {
+    case Pawn => 100
+    case Knight => 350
+    case Bishop => 350
+    case Rook => 525
+    case Queen => 1000
+    case King => 0
+  }.sum
+  board.pieces.partition(_.side == White) match { case (w, b) => value(w) - value(b) }
+}
 
-def evaluate(board: Board): Int =
-  board.pieces.partition(_.side == board.turn) match { case (cur, opp) => value(cur) - value(opp) }
+val evaluations = new ConcurrentHashMap[Vector[Piece], Int].asScala
 
-def greedyBfs(board: Board, limit: Int): Board =
+def bfs(board: Board, limit: Int): Board = {
+  evaluations.getOrElseUpdate(board.pieces, score(board))
   if (limit > 0)
-    board.moves.map { m =>
-      val after = board.move(m).get
-      if (-evaluate(after) < evaluate(board)) board
-      else greedyBfs(after, limit -1)
-    }.maxBy(evaluate)
+    if (board.moves.isEmpty) board
+    else board.moves.par.map(m => bfs(board.move(m).get, limit - 1)).maxBy(b => evaluations(b.pieces))
   else board
+}
 
-val max = greedyBfs(Board.initial, 4)
-println("Score: "+evaluate(max))
+val max = bfs(Board.initial, 5)
+println("Evaluations: "+evaluations.size)
+println("Score: "+evaluations(max.pieces))
 println("Moves: "+max.moveHistory.map(_.print).reduce(_+", "+_))
 println(max.print)
