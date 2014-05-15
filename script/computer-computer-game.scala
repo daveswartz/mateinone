b732ba6dc70cb48bad93e3d01930b65a9c79e6b1
Dave Swartz <dwswartz@gmail.com>
2014-05-15 18:01:34 -0400

    Simplified computer-computer game script.

--- script/computer-computer-game.scala
+++ script/computer-computer-game.scala
@@ -2,38 +2,27 @@ import mateinone._
 import scala.annotation.tailrec
 import TerminalPrinter._
 
-def score(board: Board): Int = {
-  def value(pieces: Vector[Piece]) = pieces.map(_.`type`).map {
-    case Pawn => 100
-    case Knight => 350
-    case Bishop => 350
-    case Rook => 525
-    case Queen => 1000
-    case King => 0
-  }.sum
-  board.pieces.partition(_.side == White) match { case (w, b) => value(w) - value(b) }
-}
+def score(piece: Piece): Int =
+  piece.`type` match { case Pawn => 100; case Knight => 350; case Bishop => 350; case Rook => 525; case Queen => 1000; case King => 0 }
 
-def minimax(node: Board, depth: Int, maximizing: Boolean): Int =
+def score(board: Board): Int =
+  board.pieces.map(p => (if (board.turn == p.side) 1 else -1) * score(p)).sum
+
+def score(node: Board, depth: Int): Int =
   if (depth == 0 || node.boards.isEmpty) score(node)
-  else if (maximizing) node.boards.map(minimax(_, depth - 1, maximizing = false)).max
-  else node.boards.map(minimax(_, depth - 1, maximizing = true)).min
+  else node.boards.map(-score(_, depth - 1)).max
 
-def nextMove(node: Board, maximizing: Boolean): MoveBase =
-  if (maximizing) node.boards.maxBy(minimax(_, 1, !maximizing)).history.last._1
-  else node.boards.minBy(minimax(_, 1, !maximizing)).history.last._1
+def nextMove(node: Board, depth: Int): MoveBase =
+  node.boards.maxBy(-score(_, depth - 1)).history.last._1
 
-@tailrec def printAndMove(index: Int, board: Board, maximizing: Boolean, last: Seq[MoveBase]) {
-  if (index > 1) println(index/2+". "+last.fold("")(_+" "+_))
+@tailrec def step(board: Board) {
   println(board.print)
   if (board.isCheckmate) println("Checkmate "+board.turn.other.toString+" wins")
   else if (board.isStalemate) println("Stalemate")
   else if (board.isInsufficientMaterial) println("Insufficient mating material")
   else if (board.isThreefoldRepetition) println(board.turn.toString+" claimed draw by threefold repetition")
   else if (board.isFiftyMoveRule) println(board.turn.toString+" claimed draw by fifty-move rule")
-  else {
-    val next = nextMove(board, maximizing)
-    printAndMove(index + 1, board.move(next).get, !maximizing, if (last.size == 2) Seq(next) else last :+ next)
-  }
+  else step(board.move(nextMove(board, 2)).get)
 }
-printAndMove(1, Board.initial, maximizing = true, Seq())
+
+step(Board.initial)
