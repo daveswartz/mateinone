237bbbb65f19429a6df0fc526ff61090b25cc44a
Dave Swartz <dwswartz@gmail.com>
2014-06-08 21:48:09 -0400

    Reuse square instances.

--- src/main/scala/mateinone/Square.scala
+++ src/main/scala/mateinone/Square.scala
@@ -14,6 +14,7 @@ object Rank { // TODO make value class
 
 object Square {
   val squares: Vector[Vector[Square]] = File.files.map(f => Rank.ranks.map(r => Square(f, r)))
+  val outside: Square = Square(-1, -1)
   private val squareIter = squares.flatten.toIterator
   val A1, A2, A3, A4, A5, A6, A7, A8,
       B1, B2, B3, B4, B5, B6, B7, B8,
@@ -23,10 +24,10 @@ object Square {
       F1, F2, F3, F4, F5, F6, F7, F8,
       G1, G2, G3, G4, G5, G6, G7, G8,
       H1, H2, H3, H4, H5, H6, H7, H8 = squareIter.next()
-  def square(file: Int, rank: Int): Square = squares(file)(rank)
+  def square(file: Int, rank: Int): Square = if (file > 7 || file < 0 || rank > 7 || rank < 0) outside else squares(file)(rank)
   private val fileStrings = Vector("a", "b", "c", "d", "e", "f", "g", "h")
 }
 case class Square private(file: Int, rank: Int) {
-  def +(rhs: (Int, Int)): Square = Square(file + rhs._1, rank + rhs._2)
+  def +(rhs: (Int, Int)): Square = Square.square(file + rhs._1, rank + rhs._2)
   override def toString: String = Square.fileStrings(file) + (rank + 1).toString
 }
